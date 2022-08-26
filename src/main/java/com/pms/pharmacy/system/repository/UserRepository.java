dbec2773ab5b40742cf4d424a177f5476ab125e8
Ghostwir3 <76448339+Ghostwir3@users.noreply.github.com>
2022-08-26 21:48:31 +0530

    user services

--- src/main/java/com/pms/pharmacy/system/repository/UserRepository.java
+++ src/main/java/com/pms/pharmacy/system/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.pms.pharmacy.system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+
 }
